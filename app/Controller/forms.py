0908214563680ff6dbb18cd3c655a33a42949915
JonBuwembo <122390422+JonBuwembo@users.noreply.github.com>
2024-04-02 22:31:24 -0400

    Added applicationForm to Forms.py so referance faculty members can be considered in the application and a statement of interest can be written

--- app/Controller/forms.py
+++ app/Controller/forms.py
@@ -5,19 +5,14 @@ from app.Model.models import Student
 from wtforms_sqlalchemy.fields import QuerySelectField
 from flask_login import current_user
 
-class EditRegForm(FlaskForm):
+
+class applicationForm():
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    address = TextAreaField('Address', [Length(min=0, max=200)]) # TextAreaField makes an adjustedable textbox
-    password = PasswordField('Password', validators=[DataRequired()])
-    topics_of_interest = TextAreaField('Topics of Interest')
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')]) # make sure passwords are the same
-    submit = SubmitField('Submit')
+    statement_of_interest = TextAreaField("Body",  validators=[DataRequired(), Length(max=1500)])
+    reference_faculty_firstname = StringField('Reference First Name', validators=[DataRequired()])
+    reference_faculty_lasntame = StringField('Reference Last Name', validators=[DataRequired()])
+    reference_faculty_email = StringField('Reference Email', validators=[DataRequired()])
 
-    def validate_email(self, email):
-        students = Student.query.filter_by(email = email.data).all()
-
-        for student in students: 
-            if (student.id != current_user.id):
-                raise ValidationError('The email is associated with another account! Use a different email address')
+    # TODO Jonathan(self): possibly write code to check if the email is valid.
+    # TODO Jonathan(self): Write code logic for getting a professor to send a letter of recommendation
