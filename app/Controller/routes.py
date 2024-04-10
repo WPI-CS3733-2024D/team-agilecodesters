7150fa4957172bf3d39a56c55af509a2834c369c
connor-jason <cejason@wpi.edu>
2024-04-09 22:53:31 -0400

    Fixed issue with index page that I caused

--- app/Controller/routes.py
+++ app/Controller/routes.py
@@ -20,6 +20,7 @@ def index_student():
     # add logic to filter out research positions based on searches.
     # research positions that align with student queried information [search feature] will show on the screen.
     search_form = SearchForm()
+    posts = ResearchPosition.query.order_by(ResearchPosition.startDate.desc())
     if search_form.validate_on_submit():
         if search_form.get_choices()[2] == search_form.sortOrder.data: #Research Fields
             # Query the ResearchPosition objects that share at least one field with the student
@@ -29,7 +30,7 @@ def index_student():
             posts = ResearchPosition.query.order_by(ResearchPosition.wantedGPA.desc())
         else: #Start date by default
             posts = ResearchPosition.query.order_by(ResearchPosition.startDate.desc())
-    return render_template('index_student.html', title='Student Home', posts=posts, search_form = search_form)
+    return render_template('index_student.html', title='Student Home', posts=posts.all(), search_form = search_form)
 
 @routes_blueprint.route('/index/faculty', methods=['GET', 'POST'])
 @login_required
