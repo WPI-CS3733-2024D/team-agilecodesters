a01eee8320773bd37e33f939c7ad26da631935c8
connor-jason <connor-jason@users.noreply.github.com>
2024-04-17 16:43:50 +0000

    Prettified Code!

--- app/Controller/routes.py
+++ app/Controller/routes.py
@@ -394,13 +394,17 @@ def other_profile(user_id):
             flash("Only student profiles can be viewed from a faculty profile!")
             return redirect(url_for("routes.index"))
         else:
-            return render_template("view_other_profile.html", user=student, user_type="Student")
+            return render_template(
+                "view_other_profile.html", user=student, user_type="Student"
+            )
     elif faculty:
         if current_user.user_type != "Student":
             flash("Only faculty profiles can be viewed from a student profile!")
             return redirect(url_for("routes.index"))
         else:
-            return render_template("view_other_profile.html", user=faculty, user_type="Faculty")
+            return render_template(
+                "view_other_profile.html", user=faculty, user_type="Faculty"
+            )
     else:
         flash("User does not exist!")
         return redirect(url_for("routes.index"))
