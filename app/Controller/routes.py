2604c32d52649c5d88382e5b686f204d766e0f71
connor-jason <cejason@wpi.edu>
2024-04-27 16:35:29 -0400

    Fixed autofilling topics of interest in edit profile but edit profile errors out when submitting? I thought we fixed that hmm

--- app/Controller/routes.py
+++ app/Controller/routes.py
@@ -373,9 +373,7 @@ def edit_profile():
             form.graduationdate.data = datetime.strptime(
                 current_user.graduationdate.strftime("%Y-%m-%d"), "%Y-%m-%d"
             ).date()
-            form.topics_of_interest.data = [
-                topic.id for topic in current_user.topics_of_interest
-            ]
+            form.topics_of_interest.data = current_user.topics_of_interest
     return render_template("edit_profile.html", title="Edit Profile", form=form)
 
 
